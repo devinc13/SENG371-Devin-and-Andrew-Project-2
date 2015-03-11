14b1cb112dbb182bdde2ca31f6838d9ec326fb73
devin <devincorrigall@gmail.com>
2015-03-11 13:02:07 -0700

    Updated readme and added in bugCounterDaily.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # SENG371-Devin-and-Andrew-Project-2
-Our repo for project 2 of Uvic's SENG 371 class.
+
+##Our Project Question: 
+For project two, we want to continue with the idea of examining the effect of progressive changes and anti-regressive changes on the number of reported bugs over time. Therefore, our question will still be:
+####"How does the number of progressive and anti-regressive changes effect the number of bugs reported over time?"
+
+##Hypotheses
+Our hypotheses are that the more progressive changes there are, the more bugs will be reported in the following weeks, and that the more anti-regressive changes there are, the less bugs will be reported over the next few months.
+
+##Methodology: 
+The above hypotheses are what we found in project 1, but our proof and explanation of these were weak, so we want to improve this.
+Instead of using our script to get all this information from github, we want to find different ways to obtain the anti-regressive, and make sure it is on a daily basis, instead of monthly. We will still use our script for counting bugs, as it seems quite reliable for that, although we will make sure that the search is limited to include only issues, not pull requests.
+For anti-regressive changes, we would like to use the tool Transit that was created by another team. This will allow us to detect both the day of an anti-regressive change, and the size of it.
+For features, we would like to still use the python script, but with some advanced searches. For repos without a "feature" label, we'd like to only search merged pull requests, and instead of searching for "feature", we will search for pull requests that don't contain the words bug, rewrite or refactor: "NOT bug AND NOT rewrite AND NOT refactor".
+
+##Codebases/Systems: 
+Ruby on Rails (6674 issues - GitHub issue tracker): https://github.com/rails/rails
+
+node.js (5608 issues - GitHub issue tracker): https://github.com/joyent/node 
+
+Bootstrap (10,820 issues - GitHub issue tracker): https://github.com/twbs/bootstrap
+
+##Metrics
+	 
+
+##Results:
+
+	
+##Analysis
+
+##Project Management Information
+
+Milestones: 
+
+Roles:
+
+
+
+
+
+
+
---
+++ bugCounterDaily.py
@@ -0,0 +1,110 @@
+import urllib2
+import re
+import time
+import sys
+import getopt
+
+usage = 'bugCounter.py -u <githubUsername> -p <githubPassword> -a <startYear> -b <startMonth> -c <endYear> -d <endMonth> -o <owner> -r <repository> -q <query> -l<label>'
+query = ''
+label = ''
+csvCount = ''
+csvDates = ''
+dateCounter = 1
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:],"hu:p:a:b:c:d:o:r:q:l:")
+except getopt.GetoptError:
+	print usage
+	sys.exit(2)
+for opt, arg in opts:
+	if opt == '-h':
+		print usage
+		sys.exit()
+	elif opt in ("-u"):
+		username = arg
+	elif opt in ("-p"):
+		password = arg
+	elif opt in ("-a"):
+		startYear = int(arg)
+	elif opt in ("-b"):
+		startMonth = int(arg)
+	elif opt in ("-c"):
+		endYear = int(arg)
+	elif opt in ("-d"):
+		endMonth = int(arg)
+	elif opt in ("-o"):
+		owner = arg
+	elif opt in ("-r"):
+		repo = arg
+	elif opt in ("-q"):
+		query = arg
+	elif opt in ("-l"):
+		label = arg
+userData = "Basic " + (username + ':' + password).encode("base64").rstrip()
+
+total = 0
+print 'Search query = ' + query
+print 'Search label = ' + label
+
+currentYear = startYear
+currentMonth = startMonth
+nextYear = startYear
+
+if startMonth == 12:
+	nextMonth = 1
+	nextYear += 1
+else:
+	nextMonth = startMonth + 1
+
+while currentYear <= endYear:
+	if currentYear == endYear:
+		if currentMonth >= endMonth:
+			break
+
+	dateRange = str(currentYear) + '-' + str(currentMonth).zfill(2) + '-01..' + str(nextYear) + '-' + str(nextMonth).zfill(2) + '-01'
+	url = 'https://api.github.com/search/issues?q=' + query.replace(" ", "+") + '+repo:' + str(owner) + '/' + str(repo) + '+created:' + dateRange
+
+	if label != '':
+		url += '+label:' + label
+		
+	req = urllib2.Request(url)
+	req.add_header("Content-type", "application/x-www-form-urlencoded")
+	req.add_header('Authorization', userData)
+	res = urllib2.urlopen(req)
+
+	p = re.compile('"total_count":(\d*)')
+
+	m = p.search(res.read())
+	total += int(m.group(1))
+	print dateRange + ' = ' + m.group(1)
+	csvCount += str(m.group(1)) + ','
+	csvDates += str(dateCounter) + ','
+	dateCounter += 1
+	if currentMonth == 12:
+		currentMonth = 1
+		currentYear += 1
+	else:
+		currentMonth += 1
+
+	if nextMonth == 12:
+		nextMonth = 1
+		nextYear += 1
+	else:
+		nextMonth += 1
+
+	time.sleep(3)
+print "Total = " + str(total)
+
+if csvCount[-1:] == ",":
+    csvCount = csvCount[:-1]
+
+if csvDates[-1:] == ",":
+	csvDates = csvDates[:-1]
+		
+print "CSV dates = " + str(csvDates)
+print "CSV count = " + str(csvCount)
+print str(startYear) + '-' + str(startMonth).zfill(2) + '-01'
+print str(endYear) + '-' + str(endMonth).zfill(2) + '-01'
+
+
+
